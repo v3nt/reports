23d0624e40d37fb8cb4243f2dcde9bfc4ca64997
v3nt <dan@jynk.net>
2023-01-15 11:35:34 +0000

    pinia added, issues fixed, eslint

--- store/index.ts
+++ store/index.ts
@@ -1,1 +1,11 @@
-import { defineStore } from '@pinia/nuxt';
+import { defineStore } from "pinia";
+
+export const useReportsStore = defineStore("reports", {
+  state: () => {
+    return {
+      reports: [],
+    };
+  },
+  actions: {},
+  getters: {},
+});
